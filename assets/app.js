bd3fab7b1dad1ab288d71599516e31decd7e41a5
Ritter Gustave <rGustave5@gmail.com>
2018-06-10 01:50:20 -0700

    An example of pulling data

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,16 @@ let make = 'ford',
     baseQueryUrl = 'https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getTrims';
 
 // request based on parameters above
-axios.get(`${baseQueryUrl}&make=${make}&model=${model}`)
-     .then( (response) => {
-        console.log(response);
+axios.get(`${baseQueryUrl}&make=${make}&model=${model}`, {
+  responseType: 'json',
+  transformResponse: [(data) => {
+    return JSON.parse(data.slice(2, data.length-2));
+  }]
+})
+     .then( response => {
+        let data = response.data;
+        console.log(data);
+     })
+     .catch( error => {
+       console.log(error);
      });
