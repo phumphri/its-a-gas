98a7a139af53c335120a34dfed40fc6481538cab
Ritter Gustave <Gustaver@uci.edu>
2018-06-12 20:38:20 -0700

    App script now pulls data according to parameters set

--- assets/app.js
+++ assets/app.js
@@ -4,53 +4,62 @@ let fs = require('fs');
 // Defining generating object
 carData = {
   modelData: [],
-  itemsProcessed: 0
 }
 
 // Temporary, key string is search parameter, change this key to generate make file name
-let key = 'Kia'
-let baseQueryUrl = 'https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getTrims';
-
+// IMPORTANT: CHANGE THE KEY AND YEARS PARAMETER ONLY
+let key = 'Ford',
+    baseQueryUrl = 'https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getTrims',
+    years = [2008, 2009, 2010, 2011, 2012],
+    dataGather
 // Using axios to hit API with a get request
 // TODO: make for loop to run trim request for years
-axios.get(`${baseQueryUrl}&make=${key}`, {
-  responseType: 'json',
-  transformResponse: [data => {
-    return JSON.parse(data.slice(2, data.length - 2))
-  }]
-}).then(response => {
-  // Data received Pushing it to object
-  response.data.Trims.forEach( (entry, index, array) => {
-    carData.modelData.push(entry);
-    carData.itemsProcessed++;
+dataGather = years.map(year => {
+  return (axios.get(`${baseQueryUrl}&make=${key}&year=${year}`, {
+    responseType: 'json',
+    transformResponse: [data => {
+      return JSON.parse(data.slice(2, data.length - 2))
+    }]
+  }));
 
-    if (carData.itemsProcessed == array.length) {
-      // console.log(carData);
-      // Call toFile function to generate the CSV file
-      ToFile(carData.modelData);
-    }
-  });
-}).catch(error => {
-  if (err) throw err;
 })
 
+Promise.all(dataGather)
+       .then( response => {
+         response.forEach( (res, index, array) => {
+           res.data.Trims.forEach( res => {
+             // console.log(res);
+             carData.modelData.push(res);
+           })
+           // console.log(res.data.Trims[0])
+         })
+         // console.log(carData.modelData);
+         ToFile(carData.modelData);
+       })
+       .catch( err => {
+         if (err) throw err;
+       })
+
+
+// =========================================================================================================
+// Function definition for file writing
 
 let ToFile = obj => {
-    const replacer = (key, value) => value === null ? "" : value;
-    const header = Object.keys(obj[0]);
+  const replacer = (key, value) => value === null ? "" : value;
+  const header = Object.keys(obj[0]);
 
-    let csv = obj.map( row => {
-        return header.map( fieldName => {
-            return JSON.stringify(row[fieldName], replacer)
-        }).join(',')
-    });
-    csv.unshift(header.join(','))
-    // console.log(csv.join('/r/n'));
+  let csv = obj.map(row => {
+    return header.map(fieldName => {
+      return JSON.stringify(row[fieldName], replacer)
+    }).join(',')
+  })
+  csv.unshift(header.join(','))
+  // console.log(csv.join('/r/n'));
 
-    fs.writeFile(`${key}.csv`, csv.join('\r\n'), err => {
-        if (err) throw err;
-        console.log(`CSV file was successfully created: ${key}.csv`);
-    })
+  fs.writeFile(`${key}.csv`, csv.join('\r\n'), err => {
+    if (err) throw err;
+    console.log(`CSV file was successfully created: ${key}.csv`);
+  })
 
-    return 0;
+  return 0;
 }
