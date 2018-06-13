bb81eb2c8ef690f2b79eb95c733a9b0ee204e644
pvhumphr <pvhumphr@usc.edu>
2018-06-13 16:29:17 -0700

    Added foreignautos.

--- app.py
+++ app.py
@@ -218,6 +218,145 @@ def manufacturer():
     
     return "The inserert was not method POST"
 
+
+
+@app.route('/foreignautos', methods=['POST', 'GET'])
+def foreignautos():
+
+    if request.method == 'GET':
+
+        conn = None
+        conn = connect_to_postgres()
+        if conn is None:
+            print("Database Connection Failed.")
+            return "Database Connection Failed"
+        else:
+            print("Database Connection Okay.")
+
+        sql = "select * from its_a_gas.foreignautos"
+
+        try:
+            cur = conn.cursor()
+            print('Cursor okay.')
+
+            cur.execute(sql)
+            print('Execute Okay.')
+
+            table_data = cur.fetchall()
+            print("Fetch All Okay")
+
+            
+            # Create json dictionary to hold metadata and table data.
+            json_dict = {}
+
+           # Add metadata that specifies schema and table.
+            json_metadata = {}
+            json_metadata["schema"] = "its_a_gas"
+            json_metadata["table"] = "foreignautos"
+            json_metadata["key"] = "month, year"
+            json_metadata["columns"] = [
+                "month", 
+                "year", 
+                "volume", 
+                "combined_volume", 
+                "adjusted_volume", 
+                "sales"]
+            json_dict['metadata'] = json_metadata
+ 
+            # Add table_data to json dictionary.
+            json_dict['table_data'] = table_data
+
+            json_object = jsonify(json_dict)
+            print("jsonify Okay")
+
+            return json_object
+
+        except Exception as e:
+            print('Execute Failed', str(e))
+            return str(e)
+
+        finally:
+            if conn is not None:
+                conn.close
+ 
+        return rows
+
+    if request.method == 'POST':
+
+        json_string = request.get_json(silent=True)
+
+        json_dict = json.loads(json_string)
+
+        json_metadata = json_dict['metadata']
+
+        print("schema:", json_metadata['schema'])
+        print("table:", json_metadata['table'])
+        # print("table_data", json_dict['table_data'])
+    
+        status_message = ""
+
+        if json_metadata['schema'] != "its_a_gas":
+            status_message = "Inncorrect schema in metadata. "
+            status_message += "Expected its_a_gas.  "
+            status_message += "Found "
+            status_message += json_metadata['schema']
+            status_message += "."
+            return status_message
+
+        if json_metadata['table'] != "foreignautos":
+            status_message = "Inncorrect table in metadata. "
+            status_message += "Expected foreignautos.  "
+            status_message += "Found "
+            status_message += json_metadata['table']
+            status_message += "."
+            return status_message
+
+        sql = "insert into " + json_metadata['schema'] + "." + json_metadata['table'] + " "
+
+        sql += "values ( "
+
+
+        for i in range(len(json_dict['table_data'][0]) - 1):
+            sql += "%s, "
+
+        sql += "%s) "
+            
+        sql += "on conflict (" + json_metadata['key'] + ") do nothing; "
+
+        conn = None
+        conn = connect_to_postgres()
+        if conn is None:
+            print("Database Connection Failed.")
+            return "Database Connection Failed"
+        else:
+            print("Database Connection Okay.")
+
+        try:
+            cur = conn.cursor()
+            print('Cursor okay.')
+
+            cur.executemany(sql, json_dict['table_data'])
+            print('Execute Many Okay.')
+
+            status_message = cur.statusmessage
+            print("cur.statusmessage:", status_message)
+
+            conn.commit()
+            print('Commit Okay.')
+
+        except Exception as e:
+            print('Execute Many Failed', str(e))
+            return str(e)
+
+        finally:
+            if conn is not None:
+                conn.close
+ 
+        return status_message        
+    
+    return "The inserert was not method POST"
+
+
 @app.route('/domesticautos', methods=['POST', 'GET'])
 def domesticautos():
 
