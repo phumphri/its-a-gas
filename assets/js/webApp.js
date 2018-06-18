db9a925eec62a346a39f9aa29df7d4e6bb22a977
Ritter Gustave <Gustaver@uci.edu>
2018-06-18 15:40:58 -0700

    Framework for webpage down

---
+++ assets/js/webApp.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+  // let queryUrl = "https://its-a-gas.herokuapp.com/models_offered_by_year";
+  // axios.get(queryUrl)
+  //      .then( response => {
+  //        console.log(response);
+  //      })
+  //      .catch( err => {
+  //        console.log(err);
+  //      })
+  console.log("System Success");
+}
